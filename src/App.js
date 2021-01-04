08c87e534ffc6c1334710b94f81409253048ce9b
Faiyaz Rafeek <fayaznahwi@gmail.com>
2021-01-04 19:47:22 +0530

    all operations added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,139 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import Button from './components/Button';
+import Input from './components/Input';
 import './App.css';
+import ClearButton from './components/ClearButton';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       input : "",
+       previousNumber : "",
+       currentNumber : "",
+       operator : ""
+    }
+  }
+  
+
+  addToInput = val =>{
+    this.setState({
+       input : this.state.input + val
+    })
+  }
+
+  addZeroToInput = val =>{
+    if (this.state.input !== ""){
+      this.setState({input: this.state.input + val})
+    }
+  }
+
+  addDecimal = val =>{
+    if (this.state.input.indexOf(".") === -1){
+      this.setState({ input : this.state.input + val })
+    }
+  }
+
+  clearInput = () =>{
+    this.setState({ input : " " })
+  }
+
+  add = () =>{
+    this.state.previousNumber = this.state.input;
+    this.setState({ input : "" });
+    this.state.operator = "plus";
+  }
+
+  minus = () =>{
+    this.state.previousNumber = this.state.input;
+    this.setState({ input : "" });
+    this.state.operator = "minus";
+  }
+
+  mul = () =>{
+    this.state.previousNumber = this.state.input;
+    this.setState({ input : "" });
+    this.state.operator = "mul";
+  }
+
+  div = () =>{
+    this.state.previousNumber = this.state.input;
+    this.setState({ input : "" });
+    this.state.operator = "div";
+  }
+
+  evaluate = () =>{
+    this.state.currentNumber = this.state.input;
+
+    switch(this.state.operator){
+      case "plus":
+        this.setState({
+          input : parseInt(this.state.previousNumber) + parseInt(this.state.currentNumber)
+        });
+        break;
+      case "minus":
+        this.setState({
+          input : parseInt(this.state.previousNumber) - parseInt(this.state.currentNumber)
+        });
+        break;
+      case "mul":
+        this.setState({
+          input : parseInt(this.state.previousNumber) * parseInt(this.state.currentNumber)
+        });
+        break;
+      case "div":
+        this.setState({
+          input : parseInt(this.state.previousNumber) / parseInt(this.state.currentNumber)
+        });
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  render() {
+    return (
+     
+        <div className="App">
+      <div className="calc-wrapper">
+        <div className="row">
+          <Input> {this.state.input}</Input>
+        </div>
+        <div className="row">
+          <Button handleClick={this.addToInput}>7</Button>
+          <Button handleClick={this.addToInput}>8</Button>
+          <Button handleClick={this.addToInput}>9</Button>
+          <Button  handleClick={this.div}>/</Button>
+        </div>
+        <div className="row">
+          <Button handleClick={this.addToInput}>4</Button>
+          <Button handleClick={this.addToInput}>5</Button>
+          <Button handleClick={this.addToInput}>6</Button>
+          <Button  handleClick={this.mul}>*</Button>
+        </div>
+        <div className="row">
+          <Button handleClick={this.addToInput}>1</Button>
+          <Button handleClick={this.addToInput}>2</Button>
+          <Button handleClick={this.addToInput}>3</Button>
+          <Button handleClick={this.add}>+</Button>
+        </div>
+        <div className="row">
+          <Button handleClick={this.addDecimal}>.</Button>
+          <Button handleClick={this.addZeroToInput}>0</Button>
+          <Button handleClick={this.evaluate}>=</Button>
+          <Button>-</Button>
+        </div>
+        <div className="row">
+          <ClearButton handleClear={this.clearInput}>Clear</ClearButton>
+        </div>
+      </div>
     </div>
-  );
+
+    )
+  }
 }
 
-export default App;
+export default App
